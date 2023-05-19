ac74c91407aade03fb36bf31c23bf0405054a071
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-05-19 12:34:23 +0300

    V2
    Firebase connection established. User login completed.

--- app/src/main/java/com/ozalp/instagram/SignInUp.java
+++ app/src/main/java/com/ozalp/instagram/SignInUp.java
@@ -1,15 +1,37 @@
 package com.ozalp.instagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.ozalp.instagram.databinding.ActivityMainBinding;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignInUp extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    EditText emailText;
+    EditText passwordText;
+    Button loginButton;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +39,49 @@ public class SignInUp extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        emailText = binding.emailText;
+        passwordText = binding.passwordText;
+        loginButton = binding.loginButton;
+        loginButton.setEnabled(true);
+
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        if(user != null){
+            Intent intent = new Intent(getApplicationContext(),DiscoveryStream.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 
     public void login(View view){
+        String emailTextData = emailText.getText().toString();
+        String passwordTextData = passwordText.getText().toString();
 
+        if(emailTextData.isEmpty() || passwordTextData.isEmpty()){
+            Toast.makeText(getApplicationContext(),
+                    "Enter email or password",
+                    Toast.LENGTH_LONG)
+                    .show();
+        }else{
+            loginButton.setEnabled(false);
+            auth.signInWithEmailAndPassword(emailTextData,passwordTextData)
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(),e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                            loginButton.setEnabled(true);
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Intent intent = new Intent(getApplicationContext(), DiscoveryStream.class);
+                            startActivity(intent);
+                            finish();
+
+                        }
+                    });
+        }
     }
 }
