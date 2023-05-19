3be9b7316d9f5cc3bde0a5712b89d3635e135d4b
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-05-19 18:14:37 +0300

    V4
    Permissions added

--- app/src/main/java/com/ozalp/instagram/DiscoveryStream.java
+++ app/src/main/java/com/ozalp/instagram/DiscoveryStream.java
@@ -1,15 +1,38 @@
 package com.ozalp.instagram;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.content.pm.PackageManager.PERMISSION_DENIED;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PackageManagerCompat;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 import com.ozalp.instagram.databinding.ActivityDiscoveryStreamBinding;
+import com.ozalp.instagram.databinding.FooterBinding;
 
 public class DiscoveryStream extends AppCompatActivity {
 
+    Uri imageData;
     ActivityDiscoveryStreamBinding binding;
+    ActivityResultLauncher <Intent> activityResultLauncher;
+    ActivityResultLauncher <String> permissionLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +41,63 @@ public class DiscoveryStream extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        registerLauncher();
+
 
     }
+
+    public void goToFile(View view){
+        if(ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+            System.out.println("PERMISSON DENIED");
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,READ_EXTERNAL_STORAGE)){
+                //-----Click link and give permission
+                Snackbar.make(view,"Not available to gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission!", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //ask permission
+                        permissionLauncher.launch(READ_EXTERNAL_STORAGE);
+                    }
+                }).show();
+                //-------
+            } else{
+                //ask permission
+                permissionLauncher.launch(READ_EXTERNAL_STORAGE);
+            }
+        }else {
+            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            activityResultLauncher.launch(intentToGallery);
+        }
+    }
+
+    private void registerLauncher(){
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode() == RESULT_OK){
+                    Intent intentFromResult = result.getData();
+
+                    if(intentFromResult != null){
+                        imageData = intentFromResult.getData();
+                        System.out.println("image url " + imageData);
+                    }
+                }
+            }
+        });
+
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if(result){
+                    Intent intentToGallery = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    activityResultLauncher.launch(intentToGallery);
+                }else {
+                    Toast.makeText(getApplicationContext(),"Permission needed!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+    }
+
+
 }
